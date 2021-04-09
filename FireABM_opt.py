7319a838abca8c6e4ced47d751259e17e589e779
Becky <25420142+beckvalle@users.noreply.github.com>
2021-04-08 19:11:37 -0500

    Update FireABM_opt.py

--- FireABM_opt.py
+++ FireABM_opt.py
@@ -266,11 +266,11 @@ def view_edge_attrib(g, attrib, fig_height=8, show_null=False, show_edge_values=
                     legend_items['names'].append('Not in list')
 
         else:
-            if ((gdf_edges[attrib].dtype == 'int64'
-                 or gdf_edges[attrib].dtype == 'float64'
-                 or gdf_edges[attrib].dtype == 'float'
-                 or gdf_edges[attrib].dtype == 'int')
-                and attrib != 'key'):
+            if ((gdf_edges[attrib].dtype == 'int64' or
+                 gdf_edges[attrib].dtype == 'float64' or
+                 gdf_edges[attrib].dtype == 'float' or
+                 gdf_edges[attrib].dtype == 'int') and
+                attrib != 'key'):
                 print('min', round(gdf_edges[attrib].min(), 2), 'max', round(gdf_edges[attrib].max(), 2))
                 ec, colors, bins = get_edge_colors_by_attr(g, attr=attrib, num_bins=num_bins, cmap=cmap, bin_cuts=breaks)
                 # print('c and b', colors, bins)
